faf3b1cde0f8989287e94fb2fd167c10099e2856
yreiss1 <reiss.yuval@gmail.com>
2018-10-18 18:01:21 -0400

    Initial Commit

---
+++ FamilyFlavors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FamilyFlavors
+//
+//  Created by Yuval Reiss on 10/18/18.
+//  Copyright © 2018 reiss.yuval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
